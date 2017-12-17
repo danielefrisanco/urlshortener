b9d9cf2e9bebd8a6c2bc468ba49e552dfa79c814
frisanco daniele <daniele@proctorexam.com>
2017-12-17 17:08:58 +0100

    more testing

--- test/index_spec.js
+++ test/index_spec.js
@@ -9,15 +9,19 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('App', function() {
-
+	// before(function(done) {
+	// 	console.log("TODO clean database")
+	// });
   describe('/shorten', function() {
-    it('whith a preferred shorten code, responds with status 201', function(done) {
+    it('responds with status 201 when a desired shortcode is not present ', function(done) {
       chai.request(app)
         .post('/shorten')
         .send({ url: 'http://example.com', shortcode: "example" })
         .end(function(err, res) {
+          console.log("must cleaan db")
           done();
 
+        	expect(res).to.be.json;
           expect(res).to.have.status(201);
           expect(res).to.have.own.property("body")
           expect(res.body).to.be.a("string");
@@ -25,11 +29,34 @@ describe('App', function() {
           done();
         });
     });
+		it('responds with status 201 with a desired shortcode that is case different', function(done) {
+      chai.request(app)
+	      .post('/shorten')
+	      .send({ url: 'http://example.com', shortcode: "case_sensitive" })
+	      .then(function(res) {
+	      	expect(res).to.have.status(201);
+	      	expect(res.body).to.equal('case_sensitive');
+	        chai.request(app)
+	          .post('/shorten')
+	      		.send({ url: 'http://example.com', shortcode: "Case_sensitive" })
+	          .end(function(err, res) {
+	            expect(res).to.have.status(201);
+	            expect(res.body).to.equal('Case_sensitive');
+	            done();
+
+	          });
+	      }).catch(function(err) {
+          console.error(err);
+	      });
+      	
+    });
+     
 		it('responds with status 201 and a shorten url', function(done) {
       chai.request(app)
         .post('/shorten')
         .send({ url: 'http://example.com' })
         .end(function(err, res) {
+        	expect(res).to.be.json;
           expect(res).to.have.status(201);
           expect(res).to.have.own.property("body")
           expect(res.body).to.be.a("string");
@@ -39,24 +66,44 @@ describe('App', function() {
     });
 
 
-    it('responds with status 400 when url is empty', function(done) {
+    it('responds with status 400 when url is not present', function(done) {
       chai.request(app)
         .post('/shorten')
         .send({ url: ' ' })
         .end(function(err, res) {
+        	expect(res).to.be.json;
           expect(res).to.have.status(400);
           done();
         });
     });
-    it('responds with status 400 when no json is passed', function(done) {
+    it('responds with status 400 when url is not present nor json', function(done) {
       chai.request(app)
         .post('/shorten')
         .send()
         .end(function(err, res) {
+        	expect(res).to.be.json;
           expect(res).to.have.status(400);
           done();
         });
     });
+    it('responds with status 409 when the desired shortcode is already in use.', function(done) {
+      chai.request(app)
+	      .post('/shorten')
+	      .send({ url: 'http://example.com', shortcode: "used" })
+	      .then(function() {
+	        chai.request(app)
+	          .post('/shorten')
+	      		.send({ url: 'http://example.com', shortcode: "used" })
+	          .end(function(err, res) {
+	            expect(res).to.have.status(409);
+	            done();
+	          });
+	      }).catch(function(err) {
+          console.error(err);
+
+	      });
+    });
+
 
   });
 });
